abb10afd95d18373a23e864dc3eebcc70627eaaf
januwA <ajanuw1641845087@sina.com>
2020-01-04 21:22:21 +0800

    修复

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const fs = require('fs')
 
+/**
+ * i: D:\ajanuw\webpack-scaffold\LICENSE
+ * o: D:\ajanuw\webpack-scaffold\dist\LICENSE
+ */
 class CopyFilePlugin {
 	constructor(files, outDir) {
 		this.files = files;
@@ -14,7 +18,7 @@ class CopyFilePlugin {
 			(webpackContext, next) => {
 				const outDir = self.outDir || webpackContext.outputOptions.path;
 				if (self.files && Array.isArray(self.files) && outDir) {
-					self.files.forEach(file => fs.copyFileSync(file, path.join(outDir, file)));
+					self.files.forEach(file => fs.copyFileSync(file, path.join(outDir, path.basename(file))));
 				}
 				next();
 			}
